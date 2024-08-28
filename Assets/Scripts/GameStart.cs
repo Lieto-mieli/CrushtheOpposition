8bbf586cd9ce0923980c73026d22f8e9f530123b
Lieto-mieli <lieto.malmiola@outlook.com>
2024-08-28 09:37:05 +0300

    added more of the UI

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    private float currentSpeed;
+    public void changevalue(float value)
+    {
+        currentSpeed = value;
+    }
     public void SwitchToGame()
     {
+        GameSpeedValue.gameSpeed = currentSpeed;
         SceneManager.LoadScene(sceneName: "Ingame");
     }
     
