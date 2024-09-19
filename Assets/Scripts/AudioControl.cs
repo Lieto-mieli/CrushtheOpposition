2c01f8abbc6b6a34d81c36d12ae9d0f627f768c5
Lieto-mieli <lieto.malmiola@outlook.com>
2024-09-19 12:12:41 +0300

    done

--- Assets/Scripts/AudioControl.cs
+++ Assets/Scripts/AudioControl.cs
@@ -36,4 +36,19 @@ public class AudioControl : MonoBehaviour
         audioMixer.SetFloat("SfxVolumeParam", Mathf.Log10(tempnum)*20);
         PlayerPrefs.SetFloat("SfxVolume", tempnum);
     }
+    public void UpdateVolume()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            audioMixer.SetFloat("MusicVolumeParam", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume")) * 20);
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        else { audioMixer.SetFloat("MusicVolumeParam", Mathf.Log10(0.5f) * 20); }
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            audioMixer.SetFloat("SfxVolumeParam", Mathf.Log10(PlayerPrefs.GetFloat("SfxVolume") * 20)); ;
+            sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume");
+        }
+        else { audioMixer.SetFloat("SfxVolumeParam", Mathf.Log10(0.5f) * 20); ; }
+    }
 }
